cfd4a444d479a2ed702220cc22afb943f4040773
Laura <poleuca.laura@gmail.com>
2023-08-11 11:17:17 +0300

    Tasks are stored in localStorage

--- task-management-app/app.js
+++ task-management-app/app.js
@@ -1,14 +1,49 @@
-    const addTaskButton = document.getElementById("addButton");
-    const dialogBox = document.getElementById("dialog");
+    const openFormButton = document.getElementById("openFormButton");
+    const taskForm = document.getElementById("taskForm");
     const titleInput = document.getElementById("title");
     const descriptionInput = document.getElementById("description");
     const assigneeInput = document.getElementById("assignee");
     const confirmButton = document.getElementById("confirmButton");
     const toDoSection = document.getElementById("toDoSection");
 
-    function getInput(){
-        toDoSection.innerHTML = titleInput.value + " " + descriptionInput.value + " " + assigneeInput.value; 
+    function addTask(){
+        const title =  titleInput.value;
+        const description = descriptionInput.value
+        const assignee = assigneeInput.value;
+
+        let task = {
+            title : title,
+            description : description,
+            assignee : assignee
+        }
+
+        let tasks;
+        const taskElement = document.createElement("div");
+        const storedTasks = localStorage.getItem("tasks");
+
+        if (storedTasks != null){
+            tasks = JSON.parse(storedTasks);
+        }
+        else{
+            tasks = [];
+        }
+
+        tasks.push(task);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        taskElement.innerHTML = task.title + " " + task.description + " " + task.assignee;
+        toDoSection.appendChild(taskElement);
+
+        closeTaskForm();
     }
 
-    addTaskButton.addEventListener("click", () => {dialogBox.showModal();});
-    confirmButton.addEventListener("click", getInput);
+    function openTaskForm(){
+        taskForm.showModal();
+    }
+
+    function closeTaskForm(){
+        taskForm.close();
+    }
+
+    openFormButton.addEventListener("click", openTaskForm);
+    confirmButton.addEventListener("click", addTask);
